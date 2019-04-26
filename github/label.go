4ba2ad5dd5ab2b016c087857375856220ba82bff
Anthony HAMON <anthony.hamon@iadvize.com>
2019-04-26 21:10:55 +0200

    github : add github client

--- github/label.go
+++ github/label.go
@@ -1,9 +1,28 @@
 package github
 
+import (
+	"context"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+)
+
 type SemverLabelService struct {
+	owner      string
+	repository string
+	client     *github.Client
 }
 
 // NewSemverLabelService creates a new instance of SemverLabelService
 func NewSemverLabelService(owner string, repository string, token string) SemverLabelService {
-	return SemverLabelService{}
+	return SemverLabelService{
+		owner,
+		repository,
+		github.NewClient(
+			oauth2.NewClient(
+				context.Background(),
+				oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token}),
+			),
+		),
+	}
 }
