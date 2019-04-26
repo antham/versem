72c530dffc184cdb1ceb1ebbc438a29ccd8582fa
Anthony HAMON <anthony.hamon@iadvize.com>
2019-04-26 20:40:12 +0200

    github : add SemverLabelService

---
+++ github/label.go
@@ -0,0 +1,9 @@
+package github
+
+type SemverLabelService struct {
+}
+
+// NewSemverLabelService creates a new instance of SemverLabelService
+func NewSemverLabelService(owner string, repository string, token string) SemverLabelService {
+	return SemverLabelService{}
+}
