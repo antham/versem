189882855a0c1e232d1f3d33bd3d3f94290ca356
Anthony HAMON <anthony.hamon@iadvize.com>
2019-04-26 20:21:50 +0200

    root : add cobra

---
+++ cmd/root.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "versem",
+	Short: "Semver manager",
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+}
+
+func initConfig() {
+	viper.AutomaticEnv()
+}
--- main.go
+++ main.go
@@ -1,3 +1,7 @@
 package main
 
-func main() {}
+import "github.com/antham/versem/cmd"
+
+func main() {
+	cmd.Execute()
+}
