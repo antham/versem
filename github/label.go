c431b1ae0d1af9f674e48d8f9e19bcf9f2303652
Anthony HAMON <anthony.hamon@iadvize.com>
2019-04-26 21:48:04 +0200

    github : add semver versions

--- github/label.go
+++ github/label.go
@@ -7,6 +7,36 @@ import (
 	"golang.org/x/oauth2"
 )
 
+// Version represents a semver version
+// NORELEASE => no version
+// PATCH     => 0.0.1
+// MINOR     => 0.1.0
+// MAJOR     => 1.0.0
+// ALPHA     => 1.0.0-alpha
+// BETA      => 1.0.0-beta
+// RC        => 1.0.0-rc
+type Version int
+
+//go:generate stringer -type=Version
+const (
+	// UNVALID_VERSION is default when no semver version exists
+	UNVALID_VERSION Version = iota
+	// NORELEASE represents for instance 1.0.0-rc
+	NORELEASE
+	// ALPHA represents for instance 1.0.0-alpha
+	ALPHA
+	// BETA represents for instance 1.0.0-beta
+	BETA
+	// RC represents for instance 1.0.0-rc
+	RC
+	// PATCH represents for instance 0.0.1
+	PATCH
+	// MINOR represents for instance 0.1.0
+	MINOR
+	// MAJOR represents for instance 1.0.0
+	MAJOR
+)
+
 type SemverLabelService struct {
 	owner      string
 	repository string
