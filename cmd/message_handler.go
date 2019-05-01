06e12ace09e2f16e8f9ae3f17db11920b8edcbe4
Anthony HAMON <anthony.hamon@iadvize.com>
2019-05-01 20:14:33 +0200

    cmd : remove unused func

--- cmd/message_handler.go
+++ cmd/message_handler.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"fmt"
 	"io"
 	"log"
 	"os"
@@ -26,17 +25,9 @@ func newMessageHandler() messageHandler {
 	return messageHandler{os.Exit, os.Stdout, os.Stderr}
 }
 
-func (m messageHandler) print(str string, args ...interface{}) {
-	failOnFprintError(fmt.Fprintf(m.stdoutWriter, str, args...))
-}
-
 func (m messageHandler) errorFatal(err error) {
 	failOnFprintError(color.New(color.FgRed).Fprint(m.stderrWriter, err.Error()+"\n"))
 	m.exit(1)
-}
-
-func (m messageHandler) error(err error) {
-	failOnFprintError(color.New(color.FgRed).Fprint(m.stderrWriter, err.Error()+"\n"))
 }
 
 func (m messageHandler) errorFatalStr(err string, args ...interface{}) {
