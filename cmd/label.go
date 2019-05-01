d69bc89c8d1b6ad0b1a6dec492d27f9feab5dc74
Anthony HAMON <anthony.hamon@iadvize.com>
2019-05-01 20:58:04 +0200

    cmd : update description

--- cmd/label.go
+++ cmd/label.go
@@ -9,7 +9,7 @@ import (
 // labelCmd represents the label command
 var labelCmd = &cobra.Command{
 	Use:   "label",
-	Short: "Interact with pull request labels",
+	Short: "Manage pull request labels",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println(cmd.Help())
 	},
